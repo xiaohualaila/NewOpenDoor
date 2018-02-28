480e61a0bd6f8f6090493d1888a459d1b6612961
xiaotaohu <380129462@qq.com>
2018-02-28 17:00:57 +0800

    修改新版本的设置读卡方式

--- app/src/main/java/ug/newopendoor/activity/setup/SetupActivity.java
+++ app/src/main/java/ug/newopendoor/activity/setup/SetupActivity.java
@@ -1,4 +1,4 @@
-package ug.newopendoor.activity;
+package ug.newopendoor.activity.setup;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,7 +18,9 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ug.newopendoor.R;
 import ug.newopendoor.activity.camera2.CameraActivity2;
+import ug.newopendoor.activity.camera6.CameraActivity6;
 import ug.newopendoor.util.FileUtil;
+import ug.newopendoor.util.SharedPreferencesUtil;
 
 
 /**
@@ -30,8 +32,6 @@ public class SetupActivity extends AppCompatActivity implements CompoundButton.O
     Switch idcard_switch;
     @BindView(R.id.scan_switch)
     Switch scan_switch;
-    @BindView(R.id.choose_switch)
-    Switch choose_switch;
     @BindView(R.id.m1_ll)
     RelativeLayout m1_ll;
     @BindView(R.id.idcard_ll)
@@ -40,23 +40,17 @@ public class SetupActivity extends AppCompatActivity implements CompoundButton.O
     TextView add_excel;
     private boolean isUitralight = true;
     private boolean isScan = true;
-    private boolean isIdcard = false;
-    private boolean isHaveThree = false;
-
-    private String path;
+    private boolean isIdcard = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
         ButterKnife.bind(this);
-        m1_ll.setVisibility(View.GONE);
         // 添加监听
         idcard_switch.setOnCheckedChangeListener(this);
         scan_switch.setOnCheckedChangeListener(this);
-        choose_switch.setOnCheckedChangeListener(this);
-        FileUtil.getPath();
-        //getExcel();
+
     }
 
 
@@ -72,9 +66,6 @@ public class SetupActivity extends AppCompatActivity implements CompoundButton.O
             case R.id.finish:
                 toActivity();
                 break;
-            case R.id.add_excel:
-
-                break;
         }
     }
 
@@ -87,50 +78,15 @@ public class SetupActivity extends AppCompatActivity implements CompoundButton.O
             case R.id.scan_switch:
                 isScan = isChecked;
                 break;
-            case R.id.choose_switch:
-                if (isChecked) {
-                    m1_ll.setVisibility(View.VISIBLE);
-                    // idcard_ll.setVisibility(View.VISIBLE);
-                    isHaveThree = true;
-                } else {
-                    m1_ll.setVisibility(View.GONE);
-                    //  idcard_ll.setVisibility(View.GONE);
-                    isHaveThree = false;
-                }
-                break;
-        }
-
-    }
-
-    //判断Excel文件是否存在
-    private void getExcel() {
-        path = FileUtil.getPath() + File.separator + "door.xls";
-        File file = new File(path);
-        if (!file.exists()) {
-            AlertDialog dialog = new AlertDialog.Builder(this)
-                    .setMessage(R.string.dialog_msg)//dialog_msg
-                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .create();
-            dialog.show();
-            return;
-        } else {//存在
-            add_excel.setText("正在导入Excel表！");
-            add_excel.setEnabled(false);
-
         }
     }
+
 
     private void toActivity() {
-        Intent intent = new Intent(this, CameraActivity2.class);
-        intent.putExtra("uitralight", isUitralight);
-        intent.putExtra("scan", isScan);
-        intent.putExtra("idcard", isIdcard);
-        intent.putExtra("isHaveThree", isHaveThree);
+        SharedPreferencesUtil.putBoolean(this,"uitralight", isUitralight);
+        SharedPreferencesUtil.putBoolean(this,"scan", isScan);
+        SharedPreferencesUtil.putBoolean(this,"idcard", isIdcard);
+        Intent intent = new Intent(this, CameraActivity6.class);
         startActivity(intent);
         finish();
     }
