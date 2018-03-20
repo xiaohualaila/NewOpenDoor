931480b6208dbb53022503d62fe82b74ab1f4a32
xiaotaohu <380129462@qq.com>
2018-03-20 13:34:43 +0800

    修改

--- app/src/main/java/ug/newopendoor/service/Service2.java
+++ app/src/main/java/ug/newopendoor/service/Service2.java
@@ -120,7 +120,7 @@ public class Service2 extends Service implements UltralightCardListener, M1CardL
                     //UltralightCard
                     if (uitralight) {
                         model.bt_seek_card(ConstUtils.BT_SEEK_CARD);
-                    //    Log.i("sss", ">>>>>>>>>>>>>>>>>>>>>>UltralightCard");
+                       Log.i("sss", ">>>>>>>>>>>>>>>>>>>>>>UltralightCard");
                         Thread.sleep(TIME);
                     } else {
                         //M1
@@ -136,7 +136,7 @@ public class Service2 extends Service implements UltralightCardListener, M1CardL
 
                     //身份证
                     if (idcard) {
-                      //  Log.i("sss", ">>>>>>>>>>>>>>>>>>>>>>身份证");
+                        Log.i("sss", ">>>>>>>>>>>>>>>>>>>>>>身份证");
                         com.decard.entitys.IDCard idCardData;
                         //标准协议
                         idCardData = BasicOper.dc_get_i_d_raw_info();
@@ -276,7 +276,7 @@ public class Service2 extends Service implements UltralightCardListener, M1CardL
                 while ((ComData = QueueList.poll()) != null) {
                     try {
                         ticketNum = new String(ComData.bRec).trim();
-                      //  Log.i("sss",">>>>>>>>>>>>>>>>"+ticketNum);
+                        Log.i("sss",">>>>>>>>>>>>>>>>"+ticketNum);
                         Ticket ticket = new Ticket(2, ticketNum);
                         RxBus.getDefault().post(ticket);
                         Thread.sleep(800);
