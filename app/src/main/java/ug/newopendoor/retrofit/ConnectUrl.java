ad3ffe9fd0d99ba8e044f43307240eab2edb9be4
xiaotaohu <380129462@qq.com>
2018-04-14 15:49:57 +0800

    修改

--- app/src/main/java/ug/newopendoor/retrofit/ConnectUrl.java
+++ app/src/main/java/ug/newopendoor/retrofit/ConnectUrl.java
@@ -8,7 +8,7 @@ public class ConnectUrl {
     /**
      * 请求服务器的接口，端口在此处修改
      */
-   public static final String URL = "http://116.62.57.42/ticket_checking/Api/api39/";
+   public static final String URL = "http://116.62.57.42/ticket_checking/Api/";
 
 
 }
