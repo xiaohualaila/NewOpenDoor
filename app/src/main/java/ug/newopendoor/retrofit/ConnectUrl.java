a651c4d1b49974fc71ef4db64e8260927bf20465
xiaotaohu <380129462@qq.com>
2018-04-14 17:25:44 +0800

    修改

--- app/src/main/java/ug/newopendoor/retrofit/ConnectUrl.java
+++ app/src/main/java/ug/newopendoor/retrofit/ConnectUrl.java
@@ -5,9 +5,6 @@ package ug.newopendoor.retrofit;
  */
 public class ConnectUrl {
 
-  // public static final String URL = "http://192.168.0.62:8081/ticket_checking/Api/api39/";//本地
-   public static final String URL = "http://116.62.57.42/ticket_checking/Api/api39/";
- //   public static final String URL = "http://47.98.49.11:8081/Api/api30/";
-    public static final String URL_IMG = "";
+   public static final String URL = "http://192.168.0.5/ticket_checking/Api/";//本地
 
 }
