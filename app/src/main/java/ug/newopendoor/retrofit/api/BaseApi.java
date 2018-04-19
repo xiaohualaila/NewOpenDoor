1658954be4554953ec7c9cfb8f6c44feccf354cb
xiaotaohu <380129462@qq.com>
2018-04-19 17:42:45 +0800

    修改

--- app/src/main/java/ug/newopendoor/retrofit/api/BaseApi.java
+++ app/src/main/java/ug/newopendoor/retrofit/api/BaseApi.java
@@ -5,6 +5,8 @@ import org.json.JSONObject;
 import java.util.List;
 
 import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -24,7 +26,6 @@ public interface BaseApi {
             @Query("qrCodeId") String qrCodeId,
             @Part List<MultipartBody.Part> file
     );
-
 
 }
 
